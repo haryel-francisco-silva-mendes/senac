b231e7924a56852371cf43bb20f3c966a4e6437f
HARYEL FRANCISCO SILVA MENDES <haryelfsm@hotmail.com>
2019-05-30 10:13:10 -0300

    aulas-html

---
+++ aulas-html/pegar-no-pendrive/projeto_01/modelo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title>Modelo</title>
+        <!--<link rel="stylesheet" href="css/modelo.css" type="text/css">-->
+    </head>
+    <body>
+        <h1>Título</h1>
+        <p>Parágrafo 1</p>
+        <p>Parágrafo 2</p>
+        <p>Parágrafo 3</p>
+        <p><?php echo 1 + 1; ?></p>
+    </body>
+</html>
